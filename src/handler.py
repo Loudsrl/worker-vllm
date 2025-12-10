5a089239669c4e371e7655e948d9abaa8ca327ae
Samuele Ferri <samuelexferri@gmail.com>
2025-12-10 12:17:11 +0100

    Update vLLM to 0.12.0, Update Hub Config, Update Handler, Remove Model Config in Engine

--- src/handler.py
+++ src/handler.py
@@ -1,22 +1,40 @@
-import os
-import runpod
-from utils import JobInput
-from engine import vLLMEngine, OpenAIvLLMEngine
+import logging
 
-vllm_engine = vLLMEngine()
-OpenAIvLLMEngine = OpenAIvLLMEngine(vllm_engine)
+import runpod
+
+from engine import OpenAIvLLMEngine, vLLMEngine
+from utils import JobInput
+
+vllm_engine = None
+openai_engine = None
+
 
 async def handler(job):
+    logging.info(f"Handler received job: {job}")
     job_input = JobInput(job["input"])
-    engine = OpenAIvLLMEngine if job_input.openai_route else vllm_engine
-    results_generator = engine.generate(job_input)
-    async for batch in results_generator:
-        yield batch
+    engine = openai_engine if job_input.openai_route else vllm_engine
+    if engine is None:
+        logging.error("Engine is None!")
+        return {"error": "Engine not initialized"}
 
-runpod.serverless.start(
-    {
-        "handler": handler,
-        "concurrency_modifier": lambda x: vllm_engine.max_concurrency,
-        "return_aggregate_stream": True,
-    }
-)
+    results_generator = engine.generate(job_input)
+
+    if job_input.stream:
+        return results_generator
+    else:
+        results = []
+        async for batch in results_generator:
+            results.append(batch)
+        return results
+
+
+if __name__ == "__main__":
+    vllm_engine = vLLMEngine()
+    openai_engine = OpenAIvLLMEngine(vllm_engine)
+
+    runpod.serverless.start(
+        {
+            "handler": handler,
+            "concurrency_modifier": lambda x: vllm_engine.max_concurrency,
+        }
+    )
